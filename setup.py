3e05df46f358545241d94df329910461abcfe41a
Aldo <aldo.gonzalez328@gmail.com>
2017-07-09 18:48:35 -0700

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='Password Generator',
+    version='0.1',
+    packages=['PasswordGen'],
+    url='https://github.com/AldoJG/PasswordGen',
+    license='MIT',
+    author='AldoJG',
+    author_email='',
+    description='A command line password generator'
+)
